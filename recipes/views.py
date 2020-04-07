2583f142e94ed03b75509b37c6e1f2298cc22123
Meg <isallari.megi@gmail.com>
2020-04-07 20:14:57 +0300

    Add basic search functionality

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
-from django.views.generic.edit import CreateView
+from django.views import generic
+from django.db.models import Count
+from django.db.models import Q
 from .models import Recipe, Ingredient
 from .forms import RecipeForm
-from django.views import generic
 
-class RecipeCreate(CreateView):
-    
+
+class RecipeCreate(generic.CreateView):
     model = Recipe
     form_class = RecipeForm
     template_name = 'recipes/recipe_form.html'
 
-    def get(self,request):
+    def get(self, request):
         form = self.form_class(None)
         return render(request, self.template_name, {'form': form})
 
@@ -24,15 +25,21 @@ class RecipeCreate(CreateView):
             return redirect('recipes:index')
         return render(request, self.template_name, {'form': form})
 
-class IndexView(generic.ListView):
 
-    template_name =  'recipes/index.html'
+class IndexView(generic.ListView):
+    template_name = 'recipes/index.html'
 
     def get_queryset(self):
-        return Recipe.objects.all()
+        queryset = Recipe.objects.all()
+        query_string = self.request.GET.get('q')
 
+        if query_string is not None:
+            queryset = queryset.filter(Q(name__icontains=query_string) | Q(description__icontains=query_string))
 
+        return queryset
+
+    
 class RecipeDetail(generic.DetailView):
     model = Recipe
-    template_name =  'recipes/recipe_detail.html'
+    template_name = 'recipes/recipe_detail.html'
 
