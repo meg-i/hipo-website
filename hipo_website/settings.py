1a17e0ce9a9364191698dfeaa6495d1bcd3bbe53
Meg <isallari.megi@gmail.com>
2020-04-09 13:29:02 +0300

    Update settings

--- hipo_website/settings.py
+++ hipo_website/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -149,3 +149,5 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 ALLOWED_HOSTS = ['hipowebsite.herokuapp.com']
+
+django_heroku.settings(locals())
